ab9cab96768f60c3f0f74b8f4111acaea1c9f4c5
QuentinS <madxmind.mail@gmail.com>
2020-06-22 09:33:07 +0200

    Refactoring PinType & config Update+Create Pin & Nav

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PinController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods={"GET"})
      * @param  PinRepository $pinRepository
      * @return Response
      */
@@ -24,7 +26,51 @@ class PinController extends AbstractController
     }
 
     /**
-     * @Route("/pin/{id<[0-9]+>}", name="app_pin_show")
+     * @Route("/pin/create", name="app_pin_create", methods={"GET", "POST"})
+     * @param  Request $request
+     * @return Response
+     */
+    public function create(Request $request): Response
+    {
+        $pin = new Pin;
+        $form = $this->createForm(PinType::class, $pin)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($pin);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pin/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/pin/{id<[0-9]+>}/update", name="app_pin_update", methods={"GET", "PUT"})
+     * @param  Pin $pin
+     * @param  Request $request
+     * @return Response
+     */
+    public function update(Pin $pin, Request $request): Response
+    {
+        $form = $this->createForm(PinType::class, $pin, ['method' => 'PUT'])->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pin/update.html.twig', [
+            'pin' => $pin,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/pin/{id<[0-9]+>}", name="app_pin_show", methods={"GET"})
      * @param  Pin $pin
      * @return Response
      */
