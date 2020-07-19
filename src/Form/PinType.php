4c92182ac12385b581df29d88df1ae848d3c35e0
QuentinS <madxmind.mail@gmail.com>
2020-07-19 20:51:01 +0200

    All LiipImagine for Thumbnail

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -18,7 +18,7 @@ class PinType extends AbstractType
                 'required' => false,
                 'allow_delete' => true,
                 'download_uri' => false,
-                // 'imagine_pattern' => '...',
+                'imagine_pattern' => 'squared_thumbnail_small',
             ])
             ->add('title')
             ->add('description');
